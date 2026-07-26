d08cb5cf6646ce8edd00033daf1d9806d3313367
Piyush Gupta <guptapiyush1300@gmail.com>
2026-07-26 21:09:15 +0530

    Time: 15 ms (23.63%), Space: 31.6 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -5,9 +5,6 @@ public:
         sort(nums.begin(), nums.end());
         int choice1=nums[0]*nums[1]*nums[n-1];
         int choice2=nums[n-1]*nums[n-2]*nums[n-3];
-        if(choice1>=choice2){
-            return choice1;
-        }
-        return choice2;
+        return max(choice1, choice2);
     }
 };
