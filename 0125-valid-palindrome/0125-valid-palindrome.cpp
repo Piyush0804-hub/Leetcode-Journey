d1667c5faea873e0c6861c260374dc6b0a34e754
Piyush Gupta <guptapiyush1300@gmail.com>
2026-08-04 22:33:16 +0530

    Time: 0 ms (100%), Space: 10.3 MB (24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,13 +4,11 @@ public:
         string s1="";
         for(char ch:s){
             if(isalnum(ch)){
-                char low=tolower(ch);
-                s1.push_back(low);
+                s1.push_back(tolower(ch));
             }
         }
-        int len=s1.length();
         int left=0;
-        int right=len-1;
+        int right=s1.length()-1;
         while(left<right){
             if(s1[left]!=s1[right]){
                 return false;
