571eb7e0bcad2821fc97cb4546ea2427a52046ac
Piyush Gupta <guptapiyush1300@gmail.com>
2026-08-06 20:03:25 +0530

    Time: 40 ms (5.59%), Space: 14.7 MB (74.81%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -6,10 +6,8 @@ public:
             mp[ch]++;
         }
         for(int i=0; i<s.length(); i++){
-            for(auto it:mp){
-                if(it.first==s[i] && it.second==1){
-                    return i;
-                }
+            if(mp[s[i]]==1){
+                return i;
             }
         }
         return -1;
