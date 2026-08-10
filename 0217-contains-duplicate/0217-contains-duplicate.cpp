8ac494a222077c07d7056001abf0e26861ffdfe2
Piyush Gupta <guptapiyush1300@gmail.com>
2026-08-11 00:31:08 +0530

    Time: 89 ms (13.73%), Space: 113.5 MB (11.34%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int x:nums){
+            m[x]++;
+        }
+        for(auto i:m){
+            if(i.second>1){
+                return true;
+            }
+        }
+        return false;
+        
+
+    }
+};
